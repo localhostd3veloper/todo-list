ec0e60c5b88a033fef70a69893e298c869a4ef37
localhostd3veloper <gautamanand022@gmail.com>
2022-03-28 12:41:35 +0530

    Code Drop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+const App = () => {
+  const [task, setTask] = useState("hi");
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Learn React",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Learn Redux",
+      isCompleted: false,
+    },
+  ]);
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-list">
+      <Header />
+      <div className="list-header">
+        <h3>TODO LIST</h3>
+        <input
+          className="shadow"
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+      </div>
+      <div className="list-body">
+        {tasks.map((task) => (
+          <div className="task" key={task.id}>
+            <input type="checkbox" />
+            <span>{task.text}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
-
+};
 export default App;
